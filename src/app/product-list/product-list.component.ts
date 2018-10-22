bf401cf4e5016304bf0d36f58997d5ef8a10c8c2
Hrithik Piyush <hrithik.is@gmail.com>
2018-10-22 20:24:51 +0530

    Part 6 updated

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -14,9 +14,9 @@ export class ProductListComponent implements OnInit {
   constructor(private _productService:ProductService) { }
 
   ngOnInit() {
-    this._productService.getProducts().subscribe(response=>{
+    this._productService.getProducts().subscribe(response =>
       this.products=response
-    });
+    );
   }
 
 }
